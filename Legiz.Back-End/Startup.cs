9ce14eb77faacb1e6d311eca9d17ba891324a18f
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2021-11-07 20:02:07 -0500

    fix: right path for Swagger

--- Legiz.Back-End/Startup.cs
+++ Legiz.Back-End/Startup.cs
@@ -65,7 +65,7 @@ namespace Legiz.Back_End
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Legiz.Back_End v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("./v1/swagger.json", "Legiz.Back_End v1"));
             }
 
             app.UseHttpsRedirection();
